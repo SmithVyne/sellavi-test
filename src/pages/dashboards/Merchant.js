04f441649d0414a3e53a8e1ee0b4f2255abee3fd
SmithVyne <smithnkereuwem2@gmail.com>
2021-11-03 13:57:20 +0300

    products

--- src/pages/dashboards/Merchant.js
+++ src/pages/dashboards/Merchant.js
@@ -1,32 +1,30 @@
 import { addDoc, collection } from '@firebase/firestore';
-import React, { useState } from 'react'
+import { ref, uploadBytes } from '@firebase/storage';
+import React, { useRef, useState } from 'react'
+import { AiOutlineCloudUpload } from 'react-icons/ai';
 import styled from 'styled-components'
-import { firestore } from '../../globals/firebase';
+import { firestore, storage } from '../../globals/firebase';
 
 const Wrapper = styled.div`
     form {
         width: 100%;
+        max-width: 300px;
         height: fit-content;
         box-sizing: border-box;
         display: flex;
         flex-direction: column;
-        align-items: center;
         gap: 20px;
         margin-top: 10px;
         padding: 30px;
-        .field {
-            display: flex;
-            flex-direction: column;
-            width: 100%;
-            input {
-                background: #F5F5F5;
-                outline: none;
-                box-sizing: border-box;
-                width: 100%;
-                height: 41px;
-                border: 1px solid ;
-                padding: 10px 0 12px 21px;
-            }
+        margin: 0;
+        padding: 0;
+        margin-top: 50px;
+        border-left: 1px solid #E31E24;
+        border-right: 1px solid #E31E24;
+        padding: 0 20px;
+        .title {
+            font-size: 22px;
+            line-height: 34px;
         }
         button {
             padding: 0 34px;
@@ -36,6 +34,7 @@ const Wrapper = styled.div`
             color: #fff;
             font-weight: 700;
             height: 41px;
+            width: fit-content;
             display: flex;
             align-items: center;
             :disabled {
@@ -46,35 +45,93 @@ const Wrapper = styled.div`
     }
 `
 
+const Grid = styled.div`
+    display: grid;
+    width: 100%;
+    gap: 20px;
+    .field {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        label{
+            margin-bottom: 10px;
+        }
+    }
+    .field.file {
+        label {
+            display: flex;
+            align-items: center;
+            gap: 5px;
+            cursor: pointer;
+            width: fit-content;
+            svg {
+                width: 30px;
+                height: 30px;
+            }
+        }
+        input{
+            display: none;
+        }
+    }
+    input, textarea {
+        outline: none;
+        box-sizing: border-box;
+        width: 100%;
+        height: 41px;
+        border: none;
+        padding: 20px;
+        box-shadow: inset 0px 0px 4px 4px #00000040;
+        border-radius: 21px;
+    }
+    .price {
+        max-width: 200px;
+        border: none;
+    }
+    .desc {
+        height: 100px;
+        padding: 15px 20px;
+    }
+`
+
 export default function Merchant() {
-    const [desc, setDesc] = useState("")
-    const [price, setPrice] = useState(0);
-    const [imageUrl, setImageUrl] = useState("");
+    const [product, updateProduct] = useState({desc: "", price: 0, imageRef: ""});
+    const [loading, setLoading] = useState(-1);
+    const fileRef = useRef();
     
     const handleSubmitProduct = (e) => {
         e.preventDefault();
         const collectionRef = collection(firestore, "products")
-        addDoc(collectionRef, {desc, price}).then(docId => console.log(docId))
+        addDoc(collectionRef, product).then(docId => console.log(docId))
+    }
+
+    const handleFileChange = ({target}) => {
+        setLoading(0);
+        const file = target.files[0];
+        const productsRef = ref(storage, "products/"+file.name)
+        uploadBytes(productsRef, file)
+        .then((snapshot) => updateProduct(val => ({...val, imageRef: snapshot.ref.fullPath})))
+        .finally(() => setLoading(1))
     }
     
     return (
         <Wrapper>
-            
             <form onSubmit={handleSubmitProduct}>
-                <span>New Product</span>
-                <div className="field">
-                    <label>Description</label>
-                    <input required type="text" value={desc} onChange={({target:{value}})=>setDesc(value)} />
-                </div>
-                <div className="field">
-                    <label>Price</label>
-                    <input required type="number" val={price} onChange={({target:{value}})=>setPrice(value)} />
-                </div>
-                <div className="field">
-                    <label>Image</label>
-                    <input type="image" />
-                </div>
-                <button>Add</button>
+                <span className="title">New product</span>
+                <Grid>
+                    <div className="field">
+                        <label>Add description</label>
+                        <textarea className="desc" required value={product.desc} onChange={({target:{value}})=>updateProduct(val => ({...val, desc: value}))} />
+                    </div>
+                    <div className="field">
+                        <label>Add price</label>
+                        <input className="price" required type="number" val={product.useRefprice} onChange={({target:{value}})=>updateProduct(val => ({...val, price: +value}))} />
+                    </div>
+                    <div className="field file">
+                        <label onClick={() => fileRef.current.click()}>Add Image <AiOutlineCloudUpload /> </label>
+                        <input ref={fileRef} type="file" onChange={handleFileChange} />
+                    </div>
+                </Grid>
+                <button disabled={loading === 0}>Add</button>
             </form>
         </Wrapper>
     )
